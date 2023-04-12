198393252e9bf39311cfaeb92c36f43bc806e451
Dng16 <dongyang0016@gmail.com>
2023-04-12 16:09:02 +0800

    initial commit

--- script.js
+++ script.js
@@ -6,7 +6,16 @@ const closeMenu = document.querySelector('.close-menu');
 const box = document.querySelector('.box');
 const menuLinks = document.querySelectorAll('.menu-links');
 const links = document.querySelector('.links');
+const overlay = document.querySelector('.overlay');
 
+
+const CloseBox = () =>{
+  overlay.classList.add('hidden');
+  closeMenu.classList.add('hidden');
+  openMenu.classList.remove('hidden');
+  box.classList.add('hide-box');
+  box.classList.remove('show-box');
+};
 
 openMenu.addEventListener('click',() =>{
 
@@ -14,15 +23,14 @@ openMenu.addEventListener('click',() =>{
   closeMenu.classList.remove('hidden');
   box.classList.add('show-box');
   box.classList.remove('hide-box');
+  overlay.classList.remove('hidden');
 });
 
-closeMenu.addEventListener('click',() =>{
 
-  openMenu.classList.remove('hidden');
-  closeMenu.classList.add('hidden');
-  box.classList.add('hide-box');
-  box.classList.remove('show-box');
-})
+closeMenu.addEventListener('click',CloseBox);
+
+overlay.addEventListener('click',CloseBox);
+
 
 for(let i = 0 ; i < menuLinks.length;i++){
   menuLinks[i].addEventListener('click',() => {
