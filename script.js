4ddd00a3648360a418fdcf2a34d5f749fdabff18
Dng16 <dongyang0016@gmail.com>
2023-04-25 18:52:34 +0800

    initial commit

--- script.js
+++ script.js
@@ -9,33 +9,20 @@ const links = document.querySelector('.links');
 const overlay = document.querySelector('.overlay');
 
 
-const CloseBox = () =>{
-  overlay.classList.add('hidden');
-  closeMenu.classList.add('hidden');
-  openMenu.classList.remove('hidden');
-  box.classList.add('hide-box');
-  box.classList.remove('show-box');
+const toggle = () =>{
+  overlay.classList.toggle('hidden');
+  closeMenu.classList.toggle('hidden');
+  openMenu.classList.toggle('hidden');
+  box.classList.toggle('show-box');
 };
 
-openMenu.addEventListener('click',() =>{
 
-  openMenu.classList.add('hidden');
-  closeMenu.classList.remove('hidden');
-  box.classList.add('show-box');
-  box.classList.remove('hide-box');
-  overlay.classList.remove('hidden');
-});
-
-
-closeMenu.addEventListener('click',CloseBox);
-
-overlay.addEventListener('click',CloseBox);
-
+menu.addEventListener('click',toggle);
+overlay.addEventListener('click',toggle);
 
 for(let i = 0 ; i < menuLinks.length;i++){
   menuLinks[i].addEventListener('click',() => {
-
-    
+  
     menuLinks[i].classList.toggle('show');
-  })
-}
+  });
+};
