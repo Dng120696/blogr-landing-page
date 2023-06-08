aa075f267947764fcfa84801de93c5ae4f7f0b07
Dng16 <dongyang0016@gmail.com>
2023-06-08 10:11:15 +0800

    final commit

--- script.js
+++ script.js
@@ -1,28 +1,33 @@
-'use strict';
+"use strict";
 
-const menu = document.querySelector('.hamburger-menu');
-const openMenu = document.querySelector('.open-menu');
-const closeMenu = document.querySelector('.close-menu');
-const box = document.querySelector('.box');
-const menuLinks = document.querySelectorAll('.menu-links');
-const links = document.querySelector('.links');
-const overlay = document.querySelector('.overlay');
+const menu = document.querySelector(".hamburger-menu");
+const openMenu = document.querySelector(".open-menu");
+const closeMenu = document.querySelector(".close-menu");
+const box = document.querySelector(".box");
+const menuLinks = document.querySelectorAll(".menu-links");
+const links = document.querySelector(".links");
+const overlay = document.querySelector(".overlay");
 
-
-const toggle = () =>{
-  overlay.classList.toggle('hidden');
-  closeMenu.classList.toggle('hidden');
-  openMenu.classList.toggle('hidden');
-  box.classList.toggle('show-box');
+const toggle = () => {
+  overlay.classList.toggle("hidden");
+  closeMenu.classList.toggle("hidden");
+  openMenu.classList.toggle("hidden");
+  box.classList.toggle("show-box");
 };
 
+menu.addEventListener("click", toggle);
+overlay.addEventListener("click", toggle);
 
-menu.addEventListener('click',toggle);
-overlay.addEventListener('click',toggle);
+menuLinks.forEach((links) => {
+  links.addEventListener("click", () => {
+    console.log(links);
+    links.classList.toggle("show");
 
-for(let i = 0 ; i < menuLinks.length;i++){
-  menuLinks[i].addEventListener('click',() => {
-  
-    menuLinks[i].classList.toggle('show');
+    menuLinks.forEach((otherlinks) => {
+      // console.log(otherlinks);
+      if (otherlinks !== links) {
+        otherlinks.classList.remove("show");
+      }
+    });
   });
-};
+});
